7f1d8eb7c631e3bffd77d289b626d9163509fce0
DucHien1712 <tranduchien509@gmail.com>
2024-11-19 08:25:28 +0700

    SOF308

--- SOF308_ASM/src/main.js
+++ SOF308_ASM/src/main.js
@@ -1,14 +1,9 @@
-// Import các file cần thiết
-import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap CSS
-import { createApp } from 'vue';  // Tạo ứng dụng Vue
-import App from './App.vue';  // Component chính của ứng dụng
-import router from './router';  // Import router từ file router.js
 
-// Tạo một ứng dụng Vue mới
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import { createApp } from 'vue'; 
+import App from './App.vue';  
+import router from './router';  
+
 const app = createApp(App);
-
-// Sử dụng router trong ứng dụng
 app.use(router);
-
-// Mount ứng dụng vào phần tử có id là 'app'
 app.mount('#app');
